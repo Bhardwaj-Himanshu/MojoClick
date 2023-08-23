6a91cb614ff1df27ba1a007529189410871a2cb3
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-08-23 23:03:25 +0530

    JS crap begins!

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//SIDEBAR
 const sidebaritem=document.querySelectorAll('.sidebar-item');
+//MESSAGES AND MESSAGE-CARD
 const messages=document.querySelector('#messages');
 const messagehighlight=document.querySelector('#message-highlight');
 const message=document.querySelectorAll('.message');
-const messagesearch=document.getElementById('message-search')
+const messagesearch=document.getElementById('message-search');
+//THEME AND CUSTOMISATION
+const theme=document.querySelector('#theme');
+const thememodal=document.querySelector('.customise-theme');
+//FONTS
+const fontsize=document.querySelector('.choose-size span')
 
 //remove active class from each item-so only one remains active at a time
 const Changeactiveitem =() =>{
@@ -46,5 +53,26 @@ messagesearch.addEventListener('keyup',()=>{
         else{
             chat.style.display='none';
         }
+    })
+})
+
+//-----------CUSTOMISE-THEME------------
+//Modal open
+theme.addEventListener('click',()=>{
+    thememodal.style.display='grid';
+    thememodal.style.boxShadow='0 0 1rem var(--color-primary)';
+})
+//Modal Close
+thememodal.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('customise-theme')){
+        thememodal.style.display='none';
+        theme.classList.remove('active');
+    }
+})
+
+//Changing the font-size using modal now
+fontsize.forEach(item=>{
+    item.addEventListener('click',()=>{
+        
     })
 })
